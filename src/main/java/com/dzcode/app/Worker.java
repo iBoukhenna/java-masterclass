00c463a53bef7701b0fb640382f86817bbb92d9e
IBoukhenna <boukhenna.ibrahim@gmail.com>
2023-04-17 19:16:36 -0400

    multi locks using synchronized code blocks step 4

--- src/main/java/com/dzcode/app/Worker.java
+++ src/main/java/com/dzcode/app/Worker.java
@@ -11,7 +11,7 @@ public class Worker {
     private List<Integer> list1 = new ArrayList<Integer>();
     private List<Integer> list2 = new ArrayList<Integer>();
 
-    public void stageOne() {
+    public synchronized void stageOne() {
         try {
             Thread.sleep(1);
         } catch (InterruptedException e) {
@@ -21,7 +21,7 @@ public class Worker {
         list1.add(random.nextInt());
     }
 
-    public void stageTwo() {
+    public synchronized void stageTwo() {
         try {
             Thread.sleep(1);
         } catch (InterruptedException e) {
