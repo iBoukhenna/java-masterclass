ce304210ca0359d40d2535c7a268cf04d142516c
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-04-17 13:12:51 -0400

    sunchronized keyword step 1

--- src/main/java/com/dzcode/app/App.java
+++ src/main/java/com/dzcode/app/App.java
@@ -1,44 +1,46 @@
 package com.dzcode.app;
 
-import java.util.Scanner;
+public class App {
 
-class Processor extends Thread {
-
-    private boolean running = true;
-
-    public void run() {
-
-        while (running) {
-
-            System.out.println("Hello ");
-
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public void shutdown() {
-        running = false;
-    }
-}
-
-public final class App {
-    private App() {
-    }
+    private int count = 0;
 
     public static void main(String[] args) {
+        App app = new App();
+        app.doWork();
+    }
 
-        Processor processor1 = new Processor();
-        processor1.start();
+    public void doWork() {
+        Thread thread1 = new Thread(new Runnable() {
 
-        System.out.println("Press return to stop ...");
-        Scanner scanner = new Scanner(System.in);
-        scanner.nextLine();
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    count++;
+                }
+            }
+        });
 
-        processor1.shutdown();
+        Thread thread2 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    count++;
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        System.out.println("Count is : " + count);
     }
 }
