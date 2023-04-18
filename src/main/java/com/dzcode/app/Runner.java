bf8752618084549b2f38325baca5ffb9d29669da
IBoukhenna <boukhenna.ibrahim@gmail.com>
2023-04-18 19:15:59 -0400

    deadlock

--- src/main/java/com/dzcode/app/Runner.java
+++ src/main/java/com/dzcode/app/Runner.java
@@ -1,57 +1,79 @@
 package com.dzcode.app;
 
-import java.util.Scanner;
-import java.util.concurrent.locks.Condition;
+import java.util.Random;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Runner {
 
-    private int count = 0;
-    private Lock lock = new ReentrantLock();
-    private Condition cond = lock.newCondition();
+    private Account acc1 = new Account();
+    private Account acc2 = new Account();
 
-    private void increment() {
-        for (int i = 0; i < 10000; i++) {
-            count++;
+    private Lock lock1 = new ReentrantLock();
+    private Lock lock2 = new ReentrantLock();
+
+    private void acquireLocks(Lock firstLock, Lock secondLock) throws InterruptedException {
+        while(true) {
+            // Acquire locks
+
+            boolean gotFirstLock = false;
+            boolean gotSecondLock = false;
+
+            try {
+                gotFirstLock = firstLock.tryLock();
+                gotSecondLock = secondLock.tryLock();
+            } finally {
+                if(gotFirstLock && gotSecondLock) {
+                    return;
+                }
+                if(gotFirstLock) {
+                    firstLock.unlock();
+                }
+                if(gotSecondLock) {
+                    secondLock.unlock();
+                }
+            }
+
+            // Locks not acquired
+            Thread.sleep(1);
         }
     }
 
     public void firstThread() throws InterruptedException {
+        Random random = new Random();
 
-        lock.lock();
+        for (int i = 0; i < 10000; i++) {
 
-        System.out.println("Waiting ...");
-        cond.await();
+            acquireLocks(lock1, lock2);
 
-        System.out.println("Woken up!");
-
-        try {
-            increment();
-        } finally {
-            lock.unlock();
+            try {
+                Account.transfer(acc1, acc2, random.nextInt(100));
+            } finally {
+                lock1.unlock();
+                lock2.unlock();
+            }
         }
     }
 
     public void secondThread() throws InterruptedException {
+        Random random = new Random();
 
-        Thread.sleep(1000);
-        lock.lock();
+        for (int i = 0; i < 10000; i++) {
 
-        System.out.println("Press the return key!");
-        new Scanner(System.in).nextLine();
-        System.out.println("Got return key!");
+            acquireLocks(lock2, lock1);
 
-        cond.signal();
-
-        try {
-            increment();
-        } finally {
-            lock.unlock();
+            try {
+                Account.transfer(acc2, acc1, random.nextInt(100));
+            } finally {
+                lock2.unlock();
+                lock1.unlock();
+            }
         }
     }
 
     public void finished() {
-        System.out.println("Count is: " + count);
+        System.out.println("Account 1 balance: " + acc1.getBalance());
+        System.out.println("Account 2 balance: " + acc2.getBalance());
+        System.out.println("Total balance: " + (acc1.getBalance() + acc2.getBalance()));
     }
 }
