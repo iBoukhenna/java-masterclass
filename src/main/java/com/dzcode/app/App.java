7a53efe1a024e3177d4344787e6534ef9f23c62a
IBoukhenna <boukhenna.ibrahim@gmail.com>
2023-04-18 12:09:16 -0400

    wait and notify step 1

--- src/main/java/com/dzcode/app/App.java
+++ src/main/java/com/dzcode/app/App.java
@@ -1,20 +1,16 @@
 package com.dzcode.app;
 
-import java.util.Random;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-
 public class App {
 
-    private static BlockingQueue<Integer> queue = new ArrayBlockingQueue<Integer>(10);
-
     public static void main(String[] args) throws InterruptedException {
+
+        final Processor processor = new Processor();
+
         Thread t1 = new Thread(new Runnable() {
             public void run() {
                 try {
-                    App.producer();
+                    processor.produce();
                 } catch (InterruptedException e) {
-                    // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
             }
@@ -23,9 +19,8 @@ public class App {
         Thread t2 = new Thread(new Runnable() {
             public void run() {
                 try {
-                    App.consumer();
+                    processor.consume();
                 } catch (InterruptedException e) {
-                    // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
             }
@@ -36,27 +31,5 @@ public class App {
 
         t1.join();
         t2.join();
-    }
-
-    private static void producer() throws InterruptedException {
-        Random random = new Random();
-
-        while(true) {
-            queue.put(random.nextInt(100));
-        }
-    }
-
-    private static void consumer() throws InterruptedException {
-        Random random = new Random();
-
-        while(true) {
-            Thread.sleep(100);
-
-            if(random.nextInt(10) == 0) {
-                Integer value = queue.take();
-
-                System.out.println("Taken value: " + value + "; Queue size is: " + queue.size());
-            }
-        }
     }
 }
