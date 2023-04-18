1f641a6edf5dfffb94f85423ee8d3751476103f6
IBoukhenna <boukhenna.ibrahim@gmail.com>
2023-04-18 17:32:33 -0400

    a wroked example using low level synchronization

--- src/main/java/com/dzcode/app/Processor.java
+++ src/main/java/com/dzcode/app/Processor.java
@@ -1,28 +1,51 @@
 package com.dzcode.app;
 
-import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Random;
 
 public class Processor {
 
+    private LinkedList<Integer> list = new LinkedList<Integer>();
+    private final int LIMIT = 10;
+    private Object lock = new Object();
+
     public void produce() throws InterruptedException {
-        synchronized (this) {
-            System.out.println("Producer thread running ...");
-            wait();
-            System.out.println("Resumed.");
+
+        int value = 0;
+
+        while(true) {
+
+            synchronized (lock) {
+
+                while(list.size() == LIMIT) {
+                    lock.wait();
+                }
+
+                list.add(value++);
+                lock.notify();
+            }
         }
     }
 
     public void consume() throws InterruptedException {
 
-        Scanner scanner = new Scanner(System.in);
-        Thread.sleep(2000);
+        Random random = new Random();
 
-        synchronized (this) {
-            System.out.println("Waiting for return key.");
-            scanner.nextLine();
-            System.out.println("Return key pressed.");
-            notify();
-            Thread.sleep(5000);
+        while(true) {
+
+            synchronized (lock) {
+
+                while(list.size() == 0) {
+                    lock.wait();
+                }
+
+                System.out.print("List size is: " + list.size());
+                int value = list.removeFirst();
+                System.out.println("; value is: " + value);
+                lock.notify();
+            }
+
+            Thread.sleep(random.nextInt(1000));
         }
     }
 }
