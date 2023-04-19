135a91fb5964a27327942c0a933f8aa42f71948a
IBoukhenna <boukhenna.ibrahim@gmail.com>
2023-04-19 08:33:51 -0400

    Semaphores step 3

--- src/main/java/com/dzcode/app/Connection.java
+++ src/main/java/com/dzcode/app/Connection.java
@@ -1,8 +1,12 @@
 package com.dzcode.app;
 
+import java.util.concurrent.Semaphore;
+
 public class Connection {
     
     private static Connection instance = new Connection();
+
+    private Semaphore semaphore = new Semaphore(20, true);
 
     private int connections = 0;
 
@@ -15,6 +19,22 @@ public class Connection {
     }
 
     public void connect() {
+        try {
+            semaphore.acquire();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        try{
+            doConnect();
+        } finally {
+            semaphore.release();
+        }
+    }
+
+    public void doConnect() {
+
         synchronized (this) {
             connections++;
             System.out.println("Current connections: " + connections);
