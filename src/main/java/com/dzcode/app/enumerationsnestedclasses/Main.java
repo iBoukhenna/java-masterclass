3235c2d34ff5e238d8f05fd761915f7f88d61d51
IBoukhenna <boukhenna.ibrahim@gmail.com>
2023-04-27 22:44:33 -0400

    nested classes step 1

--- src/main/java/com/dzcode/app/enumerationsnestedclasses/Main.java
+++ src/main/java/com/dzcode/app/enumerationsnestedclasses/Main.java
@@ -22,7 +22,30 @@ public class Main {
         for (Aircraft a : aircraft) {
             offset += a.getWakeTurbulence().getTimeOffset();
             LocalDateTime depTime = now().plusSeconds(offset);
-            System.out.println("Aircraft " + a.getModeADecimal() + " takes off at " + depTime.toLocalTime());
+            System.out.println("Aircraft " + a.getModeAOctal() + " takes off at " + depTime.toLocalTime());
         }
+
+        // inner class is just class defined in another class
+        // it has direct access to the outer class object's fields and methods
+        // because it is associated with an instance of the enclosing class it can not contain any static members
+        // to instantiate an inner class you must first instantiate the outer class then you can create an inner class object using the outer class object
+
+        // local class can be defined in a method or a for loop or if clause
+        // local class can access all the members of its enclosing class
+        // local class can access the local variables defined in the same scope; but these variables need to be final or effectively final
+        // local class can access the method parameters if they are defined within a method
+        // local class : they can not contain any static members except contancts (final static fields of primitive types or string)
+        // local class : you can not declare interfaces in a block just classes
+        // local class : they can not be instantiated from outside the block they were defined in
+        // local class : they do not have access modifiers since they are defined within a block and used within the same block
+
+        // anonymous classes : simplified local class, a great way to declare and instantiate a class at the same time
+        // anonymous class can access all the instance members of its enclosing class
+        // anonymous class can access the local variables defined in the same scope but these variable need to be final or effectively final
+        // anonymous class can access the method parameters if they are defined within a method
+        // anonymous class can contain any static members except constants (final static fields of primitive types or String)
+        // anonymous class you can not declare contructors in them
+        // anonymous class you can add extras method , fields , inner class, 
+        
     }
 }
