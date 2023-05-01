5d43ca765cfe44829e90f209df2a5ba31eed4380
IBoukhenna <boukhenna.ibrahim@gmail.com>
2023-05-01 13:05:51 -0400

    lambda expression step 4

---
+++ src/main/java/com/dzcode/app/LambdasExample04.java
@@ -0,0 +1,31 @@
+package com.dzcode.app;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class LambdasExample04 {
+
+    public static void main(String[] args) {
+        List<Product> products = ExampleData.getProducts();
+
+        BigDecimal priceLimit = new BigDecimal("5.00");
+
+        int numberOfCheapProducts = 0;
+
+        // Check if there are cheap products.
+        for (Product product : products) {
+            if (product.getPrice().compareTo(priceLimit) < 0) {
+                numberOfCheapProducts++;
+            }
+        }
+
+        // Because local variables are effectively final, you cannot modify them inside a lambda expression.
+//        products.forEach(product -> {
+//            if (product.getPrice().compareTo(priceLimit) < 0) {
+//                numberOfCheapProducts++; // Error: Variable must be effectively final
+//            }
+//        });
+
+        System.out.println("There are " + numberOfCheapProducts + " cheap products");
+    }
+}
