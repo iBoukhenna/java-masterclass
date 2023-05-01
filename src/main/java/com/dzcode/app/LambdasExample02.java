7d4d8568c376df87570acecc7888ba9be094e0bd
IBoukhenna <boukhenna.ibrahim@gmail.com>
2023-05-01 13:03:09 -0400

    lambda expression step 2

---
+++ src/main/java/com/dzcode/app/LambdasExample02.java
@@ -0,0 +1,24 @@
+package com.dzcode.app;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class LambdasExample02 {
+
+    // Print the products that cost less than the price limit.
+    static void printProducts(List<Product> products, BigDecimal priceLimit) {
+        for (Product product : products) {
+            if (product.getPrice().compareTo(priceLimit) < 0) {
+                System.out.println(product);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Product> products = ExampleData.getProducts();
+
+        BigDecimal priceLimit = new BigDecimal("5.00");
+
+        printProducts(products, priceLimit);
+    }
+}
