fb72d7bb7ae651b86464f5e66d26ebd69835ebc3
IBoukhenna <boukhenna.ibrahim@gmail.com>
2023-05-01 13:02:17 -0400

    lambda expression step 1

--- src/main/java/com/dzcode/app/App.java
+++ src/main/java/com/dzcode/app/App.java
@@ -1,17 +1,23 @@
 package com.dzcode.app;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 /**
  * Hello world!
  */
 public final class App {
-    private App() {
-    }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        // Lambda Exrpession is an anonymous method
+        // parameters : parameter types are optional
+        // parameters : no parameters : empty parentheses
+        // parameters : single parameter : parentheses are optional
+        // body : block or single expression
+
+        // Lambda Expression implements Functional Interface
+        // Functional Interface is interface with only one function
+        // Standars functional interfaces in package java.util.function
     }
 }
