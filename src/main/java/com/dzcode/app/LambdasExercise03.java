aa4fc1ca2a12b86b672dbb0afc066c3b399e7c56
IBoukhenna <boukhenna.ibrahim@gmail.com>
2023-05-01 15:41:52 -0400

    exercices

---
+++ src/main/java/com/dzcode/app/LambdasExercise03.java
@@ -0,0 +1,50 @@
+package com.dzcode.app;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LambdasExercise03 {
+
+    public interface ShoppingCartFactory {
+        ShoppingCart newShoppingCart();
+    }
+
+    /**
+     * Exercise 3a: Implement interface ShoppingCartFactory using a method reference.
+     *
+     * @return A new ShoppingCartFactory.
+     */
+    public ShoppingCartFactory getShoppingCartFactory() {
+        // TODO: Implement interface ShoppingCartFactory by using a method reference
+        // Note: Don't implement ShoppingCartFactory with an anonymous class
+        return ShoppingCart::new; // Replace 'null' by your solution
+    }
+
+    public static class ShoppingCart {
+        private final List<Product> products = new ArrayList<>();
+
+        public void add(Product product) {
+            this.products.add(product);
+        }
+
+        /**
+         * Exercise 3b: Calculate the total amount of the items in the shopping cart.
+         *
+         * @return The total amount of the items in the shopping cart.
+         */
+        public BigDecimal getTotalAmount() {
+            BigDecimal total = BigDecimal.ZERO;
+
+            // TODO: This solution does not work. Can you explain why?
+//            products.forEach(product -> total.add(product.getPrice()));
+            // total = total.add(product.getPrice())
+
+            // TODO: Implement this method in whatever way you like (you don't have to use lambda expressions)
+            for (Product product : products) {
+                total = total.add(product.getPrice());
+            }
+            return total;
+        }
+    }
+}
