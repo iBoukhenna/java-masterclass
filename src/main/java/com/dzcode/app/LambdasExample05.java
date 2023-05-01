ba69c058d2afb5a0082a8f2d1c0eb5ec63bf3ad5
IBoukhenna <boukhenna.ibrahim@gmail.com>
2023-05-01 13:19:47 -0400

    lambda expression step 5 object reference nad effectively final, stream and parallelStream

---
+++ src/main/java/com/dzcode/app/LambdasExample05.java
@@ -0,0 +1,27 @@
+package com.dzcode.app;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LambdasExample05 {
+
+    public static void main(String[] args) {
+        List<Product> products = ExampleData.getProducts();
+
+        BigDecimal priceLimit = new BigDecimal("5.00");
+
+        List<Product> cheapProducts = new ArrayList<>();
+
+        // BAD PRACTICE! Modifying cheapProducts inside the body of the lambda expression.
+        // In general, avoid side effects such as modifying objects from captured variables in lambda expressions.
+        products.forEach(product -> {
+            if (product.getPrice().compareTo(priceLimit) < 0) {
+                cheapProducts.add(product);
+            }
+        });
+
+        // Print the cheap products.
+        cheapProducts.forEach(product -> System.out.println(product));
+    }
+}
