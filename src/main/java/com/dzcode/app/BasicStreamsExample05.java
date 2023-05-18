d116e8ea748421fa313469003970e9f724ef10a4
IBoukhenna <boukhenna.ibrahim@gmail.com>
2023-05-18 06:55:45 -0400

    streams step 5 reducing and collecting streams

---
+++ src/main/java/com/dzcode/app/BasicStreamsExample05.java
@@ -0,0 +1,37 @@
+package com.dzcode.app;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BasicStreamsExample05 {
+
+    public static void main(String[] args) {
+        List<Product> products = ExampleData.getProducts();
+
+        // This is not a good way to get the elements of a stream into a list
+//        List<String> foodProductNames = new ArrayList<>();
+//        products.stream()
+//                .filter(product -> product.getCategory() == Category.FOOD)
+//                .map(Product::getName)
+//                .forEach(foodProductNames::add);
+
+        // Using collect() you can collect the elements of a stream into a collection
+        // Class Collectors contains factory methods for collectors that create different kinds of collections
+        List<String> foodProductNames = products.stream()
+                .filter(product -> product.getCategory() == Category.FOOD)
+                .map(Product::getName)
+                .collect(Collectors.toList());
+
+        System.out.println(foodProductNames);
+
+        // Collectors.joining(...) returns a collector to reduce stream elements into a string
+        String categories = products.stream()
+                .map(Product::getCategory)
+                .distinct()
+                .map(Category::name)
+                .collect(Collectors.joining("; "));
+
+        System.out.println(categories);
+    }
+}
