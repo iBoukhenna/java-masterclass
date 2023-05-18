87399c4d944a93c4a41f926f2ee540cac0097247
IBoukhenna <boukhenna.ibrahim@gmail.com>
2023-05-18 05:58:52 -0400

    exercices

---
+++ src/test/java/com/dzcode/app/FunctionalInterfacesExercise01Test.java
@@ -0,0 +1,27 @@
+package com.dzcode.app;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Predicate;
+
+import static com.dzcode.app.Category.FOOD;
+import static com.dzcode.app.TestData.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FunctionalInterfacesExercise01Test {
+
+    private FunctionalInterfacesExercise01 exercise = new FunctionalInterfacesExercise01();
+
+    @Test
+    @DisplayName("Filter products")
+    void filterProducts() {
+        assertThat(exercise.filterProducts(TestData.getProducts(), (Predicate<Product>) product -> product.getCategory() == FOOD))
+                .describedAs("Exercise 1: When searching for FOOD products, only those should be returned.")
+                .containsExactly(APPLES, SPAGHETTI, COFFEE);
+
+        assertThat(exercise.filterProducts(TestData.getProducts(), (Predicate<Product>) product -> product.getCategory() != FOOD))
+                .describedAs("Exercise 2: When searching for non-FOOD products, only those should be returned.")
+                .containsExactly(PENCILS, PLATES, DETERGENT);
+    }
+}
