ff29390facb3c27a47e3693e822117e95d0f0062
IBoukhenna <boukhenna.ibrahim@gmail.com>
2023-05-18 06:54:42 -0400

    streams step 1 understading streams

---
+++ src/main/java/com/dzcode/app/BasicStreamsExample01.java
@@ -0,0 +1,22 @@
+package com.dzcode.app;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class BasicStreamsExample01 {
+
+    public static void main(String[] args) {
+        List<Product> products = ExampleData.getProducts();
+
+        // Streams are lazy - without a terminal operation, no work is done
+        // When you call intermediate operations on a stream, you're only building the pipeline;
+        // no elements are flowing through it yet
+        Stream<Product> stream = products.stream().map(product -> {
+            System.out.println(product);
+            return product;
+        });
+
+        // When you call a terminal operation, the stream will do the work
+        stream.forEach(product -> {});
+    }
+}
